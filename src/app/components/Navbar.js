0f4ad4b089a4f340f03e0db22aedfc9d063adeaf
OM SACHIN SALUNKE <103877025+om0852@users.noreply.github.com>
2024-01-28 18:06:04 +0530

    Update Navbar.js

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -344,13 +344,13 @@ const Navbar = () => {
                                 <div className=" photo-wrapper p-2">
                                   <img
                                     className="w-32 h-32 rounded-full mx-auto"
-                                    src="https://www.gravatar.com/avatar/2acfb745ecf9d4dccb3364752d17f65f?s=260&d=mp"
-                                    alt="John Doe"
+                                    src={user.pic ? user.pic : image2.src}
                                   />
                                 </div>
                                 <div className="p-2">
                                   <h3 className="text-center text-xl text-gray-900 font-medium leading-8">
-                                    {user.name}                                  </h3>
+                                    {user.name}
+                                  </h3>
                                   <div className="text-center text-gray-400 text-xs font-semibold">
                                   </div>
                                   <table className="text-xs my-3">
@@ -360,7 +360,7 @@ const Navbar = () => {
                                           Address
                                         </td>
                                         <td className="px-2 py-2">
-                                          {user.address}                                        </td>
+                                          {user.address}                                </td>
                                       </tr>
                                       <tr>
                                         <td className="px-2 py-2 text-gray-500 font-semibold">
@@ -380,10 +380,11 @@ const Navbar = () => {
                                   <div className="text-center my-3">
                                     <a
                                       className="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium"
-                                      href="#"
+                                      href="/dashboard/profile"
                                     >
                                       View Profile
                                     </a>
+
                                   </div>
                                 </div>
                               </div>
