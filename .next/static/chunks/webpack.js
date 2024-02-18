c63fce9ddc833d4647911105d076bbb3a2c442e7
omsalunke0852 <103877025+om0852@users.noreply.github.com>
2024-02-18 11:02:32 +0530

    fund deposite and withdraw api ready

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ee0d406e4cd4fc96"; }
+/******/ 		__webpack_require__.h = function() { return "fef666788a99b9ef"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ src/app/api/portfolio/fundwithdraw/route.js
@@ -0,0 +1,29 @@
+
+import connectDB from "../../../../utils/mongoose";
+import { NextResponse } from "next/server";
+import orgTransfer from "@/models/orgTransfer";
+
+export async function POST(req, res) {
+    try {
+        const body = await req.json();
+        const { pid, transactionHash, Amount } = body;
+        await connectDB();
+        const data = { transactionHash, date: new Date(), Amount, Method: "withdraw" }
+        const FundData = await orgTransfer.findOne({ PortfolioId: pid });
+        if (FundData == null) {
+            await orgTransfer.create({
+                PortfolioId: pid,
+                Transaction: data,
+                TotalAmount: Amount
+            })
+        }
+        else {
+            FundData.Transaction.push(data)
+            await orgTransfer.updateOne({ portfolioId: pid, Transaction: FundData.Transaction, TotalAmount: (parseFloat(FundData.TotalAmount) + parseFloat(Amount)) })
+        }
+        return NextResponse.json({ status: 200, error: "successfully" });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ status: 300, error: "check internet connection" });
+    }
+}
